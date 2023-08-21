7a716912118c9621322d4668fb8e3da85de27ca3
divyakrishnan15 <divyabalajee@gmail.com>
2023-08-21 16:41:39 -0400

    thoughtRoutes

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,34 @@
+const router = require('express').Router();
+const {
+  getThoughts,
+  createThoughts,
+  getSingleThought,
+  updateThoughts,
+  deleteThoughts,
+  createReactions,
+  deleteReactions,
+} = require('../../controllers/thoughtController');
+
+//GET ALL, POST
+router.route('/')
+.get(getThoughts)
+// .post(createThoughts);
+
+
+router.route('/:thoughtId')
+.get(getSingleThought)
+.put(updateThoughts)
+.delete(deleteThoughts)
+
+router.route('/:userId').post(createThoughts);
+
+
+router.route('/:thoughtId/reactions').post(createReactions);
+
+router.route('/:thoughtId/reactions/:reactionId').delete(deleteReactions);
+
+
+
+module.exports = router;
+
+
